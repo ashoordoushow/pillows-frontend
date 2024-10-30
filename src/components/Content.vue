d38263e64fa235566d08c6c82dc50c3cc895ab5c
ashoordoushow <ashoor12323@gmail.com>
2024-10-29 21:24:10 -0500

    Vue frontend index action

---
+++ src/components/Content.vue
@@ -0,0 +1,34 @@
+<script>
+import PillowsIndex from "./PillowsIndex.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    PillowsIndex,
+  },
+  data: function () {
+    return {
+      pillows: [],
+    };
+  },
+  created: function () {
+    this.handleIndexPhotos();
+  },
+  methods: {
+    handleIndexPhotos: function () {
+      axios.get("http://localhost:3000/pillows.json").then((response) => {
+        console.log("pillows index", response);
+        this.pillows = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <PillowsIndex v-bind:pillows="pillows" />
+  </main>
+</template>
+
+<style></style>
---
+++ src/components/PillowsIndex.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    pillows: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All pillows</h1>
+    <div v-for="pillow in pillows" v-bind:key="pillow.id">
+      <h2>{{ pillow.name }}</h2>
+      <img v-bind:src="pillow.url" />
+      <p>Width: {{ pillow.width }}</p>
+      <p>Height: {{ pillow.height }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
